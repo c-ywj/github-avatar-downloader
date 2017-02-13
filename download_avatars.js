5a04571f75c06d2b3b9dabc8b0039f6eb593772e
c-ywj <cybc00@hotmail.com>
2017-02-13 23:18:22 +0000

    Modified download_avatars.js, declared and defined requestURL variable

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,17 @@ var request = require("request");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = "c-ywj";
+var GITHUB_TOKEN = "cae1cc5b0f105d51de9b8ccf7dd5f52e1eb8ce17";
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
