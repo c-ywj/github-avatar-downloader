6b0a3cfd76aabd66f181cac37e1e7a2e901a5da9
c-ywj <cybc00@hotmail.com>
2017-02-13 23:59:36 +0000

    Modified download_avatars.js, adds headers, and request method

--- download_avatars.js
+++ download_avatars.js
@@ -5,14 +5,27 @@ console.log("Welcome to the GitHub Avatar Downloader!");
 var GITHUB_USER = "c-ywj";
 var GITHUB_TOKEN = "cae1cc5b0f105d51de9b8ccf7dd5f52e1eb8ce17";
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log('result:', body) // Show the HTML for the Google homepage.
+    }
+  })
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Result:", result.body);
 });
 
 
