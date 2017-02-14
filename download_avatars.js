4318cb3ccb1ff109592448cd9785e6586156d508
c-ywj <cybc00@hotmail.com>
2017-02-14 01:11:51 +0000

    Added comments to download_avatars.js file

--- download_avatars.js
+++ download_avatars.js
@@ -7,20 +7,22 @@ console.log("Welcome to the GitHub Avatar Downloader!");
 var GITHUB_USER = "c-ywj";
 var GITHUB_TOKEN = "cae1cc5b0f105d51de9b8ccf7dd5f52e1eb8ce17";
 
+//Makes request to github API, requests for JSON file
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + '/' + repoName + '/contributors';
-
+//adds options object within the same scope as getRepoContributors, for adding headers to athenticate through error 403 FORBIDDEN error
   var options = {
     url: requestURL,
     headers: {
       'User-Agent': 'GitHub Avatar Downloader - Student Project'
     }
   };
-
+//request method, brings in options object, cb defined @ getRepoContributors execution
   request(options, cb);
 
 }
 
+//downloadImageByURL makes requests to target url and downloads files into target filePath
 function downloadImageByURL(url, filePath) {
   request.get(url)
   .on('error', function (err) {
